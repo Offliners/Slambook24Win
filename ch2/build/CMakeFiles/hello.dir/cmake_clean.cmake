60d4e63136964c095ada9b4f333a7b63c1b05df4
Offliner <eric21904@gmail.com>
2021-03-21 17:38:40 +0800

    ch2

---
+++ ch2/build/CMakeFiles/hello.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello.dir/libHelloSLAM.cpp.obj"
+  "CMakeFiles/hello.dir/libHelloSLAM.cpp.obj.d"
+  "libhello.a"
+  "libhello.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ch2/build/CMakeFiles/useHello.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/useHello.dir/useHello.cpp.obj"
+  "CMakeFiles/useHello.dir/useHello.cpp.obj.d"
+  "libuseHello.dll.a"
+  "useHello.exe"
+  "useHello.exe.manifest"
+  "useHello.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/useHello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
