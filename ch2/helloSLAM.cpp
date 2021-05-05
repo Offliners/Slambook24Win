fcc18b4946bdc42cfc3a6e4cbecad621b31e9f3b
Offliner <42777325+Offliners@users.noreply.github.com>
2021-05-05 18:00:18 +0800

    Update helloSLAM.cpp

--- ch2/helloSLAM.cpp
+++ ch2/helloSLAM.cpp
@@ -1,7 +1,7 @@
 #include<iostream>                                                                                   
 using namespace std;
 
-int main(int argc, char** argv)
+int main()
 {
         cout << "Hello SLAM!" << endl;
 
